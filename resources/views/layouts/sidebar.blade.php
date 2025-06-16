dd4782fadcaf0287a9f1d0d7a5d3ada8085db460
madushan10 <mnmnjith@gmail.com>
2025-06-16 14:56:46 +0530

    data table fucntions added

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -245,13 +245,29 @@
     </script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
+    <!--datatable responsive css-->
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
 
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+   
     <!-- Select2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>   
     <script src="https://cdn.jsdelivr.net/npm/parsleyjs@2.9.2/dist/parsley.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/parsleyjs@2.9.2/src/parsley.min.css">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
     <script>
         $(document).ready(function() {
             $('.myDropdown').select2({
@@ -259,6 +275,62 @@
                 allowClear: true
             });
         });
+    document.addEventListener('DOMContentLoaded', function () {
+       
+        const tablesToExport = document.querySelectorAll('.data-table-export');
+        const exportToggleButtons = document.querySelectorAll('.export-toggle');
+
+        tablesToExport.forEach(function (tableEl) {
+            const exportTitle = tableEl.getAttribute('data-export-title') || 'Exported Table';
+            const exportFilename = tableEl.getAttribute('data-export-filename') || 'exported_table';
+
+            new DataTable(tableEl, {
+                dom: 'Bfrtip', 
+                buttons: [
+                    {
+                        extend: 'csv',
+                        title: exportTitle,
+                        filename: exportFilename
+                    },
+                    {
+                        extend: 'excel',
+                        title: exportTitle,
+                        filename: exportFilename
+                    }
+                ]
+            });
+        });
+
+        exportToggleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            let dtButtonsContainer = null;
+
+            dtButtonsContainer = this.nextElementSibling;
+            if (dtButtonsContainer && !dtButtonsContainer.classList.contains('dt-buttons')) {
+
+                dtButtonsContainer = this.parentNode.querySelector('.dt-buttons');
+            }
+
+            if (!dtButtonsContainer || !dtButtonsContainer.classList.contains('dt-buttons')) {
+                 const commonAncestor = this.closest('.page-container, .main-scrollable, .table-controls-area'); // Add your relevant parent classes/IDs here
+                 if (commonAncestor) {
+                     dtButtonsContainer = commonAncestor.querySelector('.dt-buttons');
+                 }
+            }
+
+
+            if (dtButtonsContainer && dtButtonsContainer.classList.contains('dt-buttons')) {
+                dtButtonsContainer.classList.toggle('d-none'); // Use a CSS class to toggle visibility
+            } else {
+                console.warn("Could not find a corresponding '.dt-buttons' container for the clicked export toggle button.", this);
+            }
+        });
+    });
+    const allDtButtons = document.querySelectorAll('.dt-buttons');
+    allDtButtons.forEach(dtButton => {
+        dtButton.classList.add('d-none'); 
+    });
+    });
     </script>
 
 
--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -92,7 +92,7 @@
                                                 <h3 class="card-title">{{ __('app.products.title') }}</h3>
                                             </div>
                                             <div>
-                                                <button class="btn white-btn">
+                                                <button class="btn white-btn export-toggle">
                                                     <svg width="18" height="18" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M8.44091 4.89252C8.48096 4.93551 8.52926 4.96998 8.58293 4.99389C8.63659 5.01781 8.69453 5.03066 8.75327 5.0317C8.81201 5.03274 8.87036 5.02193 8.92484 4.99993C8.97932 4.97792 9.0288 4.94517 9.07035 4.90363C9.11189 4.86208 9.14464 4.8126 9.16665 4.75812C9.18865 4.70364 9.19946 4.64529 9.19842 4.58655C9.19738 4.52781 9.18452 4.46987 9.16061 4.41621C9.1367 4.36254 9.10222 4.31424 9.05924 4.27419L7.30924 2.52419C7.22721 2.44226 7.11601 2.39624 7.00007 2.39624C6.88414 2.39624 6.77294 2.44226 6.69091 2.52419L4.94091 4.27419C4.86363 4.35712 4.82155 4.46682 4.82355 4.58016C4.82555 4.6935 4.87147 4.80164 4.95163 4.8818C5.03179 4.96196 5.13993 5.00787 5.25327 5.00987C5.36661 5.01187 5.4763 4.9698 5.55924 4.89252L6.56257 3.88919V8.66669C6.56257 8.78272 6.60867 8.894 6.69071 8.97605C6.77276 9.05809 6.88404 9.10419 7.00007 9.10419C7.11611 9.10419 7.22739 9.05809 7.30943 8.97605C7.39148 8.894 7.43757 8.78272 7.43757 8.66669V3.88919L8.44091 4.89252Z" fill="#172635" />
                                                         <path d="M12.1041 7.5C12.1041 7.38397 12.058 7.27269 11.9759 7.19064C11.8939 7.10859 11.7826 7.0625 11.6666 7.0625C11.5506 7.0625 11.4393 7.10859 11.3572 7.19064C11.2752 7.27269 11.2291 7.38397 11.2291 7.5C11.2291 8.05538 11.1197 8.60533 10.9072 9.11843C10.6946 9.63154 10.3831 10.0978 9.99039 10.4905C9.59768 10.8832 9.13146 11.1947 8.61835 11.4072C8.10524 11.6198 7.5553 11.7292 6.99992 11.7292C6.44454 11.7292 5.89459 11.6198 5.38149 11.4072C4.86838 11.1947 4.40216 10.8832 4.00945 10.4905C3.61673 10.0978 3.30521 9.63154 3.09268 9.11843C2.88014 8.60533 2.77075 8.05538 2.77075 7.5C2.77075 7.38397 2.72466 7.27269 2.64261 7.19064C2.56056 7.10859 2.44928 7.0625 2.33325 7.0625C2.21722 7.0625 2.10594 7.10859 2.02389 7.19064C1.94185 7.27269 1.89575 7.38397 1.89575 7.5C1.89575 8.85371 2.43351 10.152 3.39073 11.1092C4.34794 12.0664 5.64621 12.6042 6.99992 12.6042C8.35363 12.6042 9.65189 12.0664 10.6091 11.1092C11.5663 10.152 12.1041 8.85371 12.1041 7.5Z" fill="#172635" />
@@ -103,7 +103,7 @@
 
                                                 </button>
 
-                                                <button class="btn white-btn">
+                                                <button class="btn white-btn" data-bs-toggle="offcanvas" data-bs-target="#offFilter" aria-controls="offcanvasRight">
                                                     <svg width="18" height="18" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M2.33333 2.25H11.6667C11.8214 2.25 11.9697 2.31146 12.0791 2.42085C12.1885 2.53025 12.25 2.67862 12.25 2.83333V3.7585C12.25 3.9132 12.1885 4.06155 12.0791 4.17092L8.33758 7.91242C8.22818 8.02179 8.1667 8.17014 8.16667 8.32483V12.0027C8.16666 12.0914 8.14645 12.1789 8.10755 12.2586C8.06866 12.3383 8.01211 12.4081 7.94221 12.4626C7.8723 12.5172 7.79088 12.5551 7.70414 12.5734C7.61739 12.5918 7.5276 12.5901 7.44158 12.5686L6.27492 12.2769C6.14877 12.2453 6.03681 12.1725 5.9568 12.07C5.87679 11.9674 5.83334 11.8411 5.83333 11.7111V8.32483C5.8333 8.17014 5.77182 8.02179 5.66242 7.91242L1.92092 4.17092C1.81151 4.06155 1.75003 3.9132 1.75 3.7585V2.83333C1.75 2.67862 1.81146 2.53025 1.92085 2.42085C2.03025 2.31146 2.17862 2.25 2.33333 2.25Z" stroke="#172635" stroke-width="0.875" stroke-linecap="round" stroke-linejoin="round" />
                                                     </svg>
@@ -120,7 +120,7 @@
                                             <form id="bulk-delete-form" method="POST" action="{{ url('delete-selected-products') }}">
                                             @csrf
                                             <div class="table-responsive">
-                                                <table class="table new-table" id="newTable">
+                                                <table class="table new-table data-table-export" id="newTable"  data-export-title="Products" data-export-filename="Products">
 
                                                     <thead>
                                                         <tr>
@@ -166,7 +166,7 @@
                                                     </tbody>
                                                 </table>
                                             </div>
-                                            <button type="submit" class="btn btn-danger btn-sm mb-2" onclick="return confirm('Are you sure you want to delete selected records?')">Delete Selected</button>
+                                            <button type="submit" class="btn btn-danger btn-xs mb-2" onclick="return confirm('Are you sure you want to delete selected records?')">Delete Selected</button>
                                             </form>               
                                         </div>
 
@@ -192,6 +192,31 @@
     });
 </script>
 
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offFilter" aria-labelledby="offFiltertLabel">
+    <div class="offcanvas-header border-bottom">
+        <h5 class="offcanvas-title" id="offFilterLabel">Filter</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body p-4 overflow-hidden">
+    <form action="" method="get" enctype="multipart/form-data">
+    <div class="row">
+        <div class="col-md-12 mb-3">
+            <label for="sku" class="form-label">SKU</label>
+            <input type="text" class="form-control" name="sku" value="{{ request('sku') }}">
+        </div>
+        <div class="col-md-12 mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" name="name" value="{{ request('name') }}">
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary btn-sm">Apply Filter</button>
+    <a href="{{ url('products') }}" class="btn btn-danger btn-sm">Clear</a>
+</form>
+
+
+    </div>
+   
+</div>
 @endsection
 
 
